9c921c67baa9e1b0795fa5f5f8eb42e4474c6d21
Devo-hacker <niladreebiharinayak@gmail.com>
2026-03-21 10:36:14 +0530

    fix: convert backend submodule to regular folder

---
+++ backend/routes/note.routes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { createNote, getNotes, updateNote, deleteNotes } from "../controllers/note.controller.js";
+
+const router = express.Router();
+router.get('/get-notes', getNotes);
+router.post("/create-note", createNote);
+router.put("/update-note/:id", updateNote);
+router.delete("/delete-note/:id", deleteNotes);
+export default router;
